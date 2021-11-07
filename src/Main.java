1d4be2952c76bbf5b2c079284889cb4c9dcd1d60
Marta Bartusch <martabartusch@gmail.com>
2021-11-07 17:19:29 +0100

    zadanie 5

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,7 @@ public class Main {
 
         String [] stringAr = new String[6];
         fillStringArray(stringAr);
+        showStringArray(stringAr);
 
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
@@ -41,6 +42,18 @@ public class Main {
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
 
+        System.out.println("Zad 3");
+
+        String myName = "Marta";
+        showMyName(myName);
+
+        String myLast = "Bartusch";
+        showMe(myName, myLast);
+
+        int myAge = 23;
+
+        showMyAge(myName, myLast, myAge);
+
         /* zad.4
         a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
             coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
@@ -49,6 +62,11 @@ public class Main {
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
 
+        System.out.println("Zad 4");
+
+        int sum = add(4,66);
+        int sum3 = add3(90,4,2);
+        System.out.println("wynik: " + (add(5, 92) + add3(251, 33, 4)));
     }
 
     public static void fillArray(int [] ar) {
@@ -67,11 +85,44 @@ public class Main {
         }
 
         public static void fillStringArray(String [] ar) {
-            for (String x: ar) {
+            System.out.println("Podaj słowa");
+            for (int i = 0; i < ar.length; i++) {
                 Scanner scan = new Scanner(System.in);
-                x = scan.next();
+                ar [i] = scan.next();
             }
+        }
+
+    public static void showStringArray(String [] ar) {
+        for (int i = ar.length; i > 0; i--)
+            System.out.println(ar[i-1]);
     }
+
+    public static void showMyName (String name)
+    {
+        System.out.println(name);
+    }
+
+    public static void showMe (String name, String lastName)
+    {
+        System.out.println(name + " " + lastName);
+    }
+
+    public static void showMyAge (String name, String lastName, int age)
+    {
+        System.out.println(name + " " + lastName + " " + age);
+    }
+
+    public static int add (int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int add3 (int a, int b, int c)
+    {
+        return a + b + c;
+    }
+
+
 }
 
 
