628f50804a90b681648bccb76aa30574b6690598
Marta Bartusch <martabartusch@gmail.com>
2021-11-07 16:41:08 +0100

    zadanie 4

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,35 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        System.out.println("Pętla A:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Mimo pieciokrotnej pętli zostanie wypisana raz, bo jest tu break");
+            break;
+        }
+
+        System.out.println("Pętla B:");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Licznik pętli: " + (i+1));
+            if(i == 0) {
+                System.out.println("Pętla wykona się 5 razy, ale to co się dzieje pod continue nie jest wykonywane");
+            }
+            else {
+                continue;
+            }
+            System.out.println("ale to wydrukuje się raz - continue jest w else");
+        }
+
+        System.out.println("Pętla C:");
+
+        for (int i = 0; i<= 0; i--) {
+            System.out.println("Gdyby nie return to pętla działałaby w nieskończoność");
+            return;
+        }
+
+
+
+
     }
 }
