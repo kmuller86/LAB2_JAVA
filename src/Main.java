365b7605b3a10eaf27d6119ee0d82a14171f62c7
Marta Bartusch <martabartusch@gmail.com>
2021-11-07 15:51:25 +0100

    zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //test
-
         /* cw.1 pętle while */
+
+        System.out.println("Zad.1");
 
         /* zad 1 - Napisać za pomocą pętli do-while kod który będzie iterował od liczby 1000 w dół do 0.
           Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indexu (podać nr indexu)
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
+        int i = 1000;
 
+        do {
+            if (i%58 == 0) {
+                System.out.printf("Liczba %d podzielna przez 58. \n", i);
+            }
+            i--;
+        } while (i >= 0 );
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
@@ -19,10 +26,43 @@ public class Main {
         c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
         */
 
+        System.out.println("Zad.2a");
+
+        int [] myArray = {5,25,6,78,13};
+
+        int j = myArray.length;
+        while (j > 0) {
+            j--;
+            System.out.println(myArray[j]);
+        }
+
+        System.out.println("Zad.2b");
+
+        double [] myDoubleArray = {5.75342,25.34,6.233312,78,13.568};
+
+        int k = myDoubleArray.length;
+        while (k > 0) {
+            k--;
+            System.out.println(myDoubleArray[k]);
+        }
+
+        System.out.println("Zad.2c");
+
+        String [] myStringArray = {"Basia", "Zosia", "Kasia", "Olek", "Maja"};
+
+        int l = myStringArray.length;
+        while (l > 0) {
+            l--;
+            System.out.println(myStringArray[l]);
+        }
         /* zad.3
         a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
         b) a jak gdy damy false --> while(false)
         */
+
+        System.out.println("Zad.3a");
+
+
 
     }
 }
